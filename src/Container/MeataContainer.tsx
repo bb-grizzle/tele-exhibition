f09bce9f18e2644ad4862c3ce0d07240f447e9a6
grizzle <firbigi1993@gmail.com>
2020-12-09 20:43:10 +0900

    add | og tag

--- src/Container/MeataContainer.tsx
+++ src/Container/MeataContainer.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { HeadData } from "../Data/Head";
 
 const MeataContainer = () => {
 	return (
 		<HelmetProvider>
 			<Helmet>
 				<meta charSet="utf-8" />
-				<title> D; | EXHIBITION</title>
+				<title>{HeadData.title}</title>
 				<link rel="stylesheet" href="https://use.typekit.net/yjq4hvv.css" />
+
+				<meta
+					name="viewport"
+					content="height=device-height, 
+				width=device-width, 
+				initial-scale=1.0, 
+				minimum-scale=1.0, 
+				maximum-scale=1.0, 
+				user-scalable=no"
+				></meta>
+				<meta property="og:title" content={HeadData.title} />
+				<meta property="og:site_name" content={HeadData.title} />
+				<meta property="og:url" content={HeadData.url} />
+				<meta property="og:description" content={HeadData.description} />
+				<meta property="og:image" content={`${HeadData.url}thumbnail.png`} />
+				<meta property="og:type" content="website" />
 			</Helmet>
 		</HelmetProvider>
 	);
---
+++ src/Data/Head.ts
@@ -0,0 +1,5 @@
+export const HeadData = {
+	description: "D-semicolon Home Exhibition",
+	title: "D; | EXHIBITION",
+	url: "https://homeexhibi.web.app/"
+};
